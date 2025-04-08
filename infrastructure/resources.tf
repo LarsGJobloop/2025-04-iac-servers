0aac2ae6dd8e73ad4ec0fc152001ad764b3f34b1
zabronax <zabronax@gmail.com>
2025-04-08 10:29:05 +0200

    setup ssh keys

--- infrastructure/resources.tf
+++ infrastructure/resources.tf
@@ -1,8 +1,15 @@
+resource "hcloud_ssh_key" "toy_server_ssh_key" {
+  name       = "toy_server_ssh_key"
+  public_key = file("./.server-demo-key.pub")
+}
+
 resource "hcloud_server" "toy_server" {
   name        = "toy-server"
   location    = "hel1"
   image       = "debian-12"
   server_type = "cax21"
+
+  ssh_keys = [hcloud_ssh_key.toy_server_ssh_key.id]
 
   public_net {
     ipv4_enabled = true
